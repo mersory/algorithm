5468c7137cb1d2aa53241e334e332a77ef5e9e28
dymxy1129@163.com <DYmxy1992#>
2018-05-23 20:42:42 +0800

    初始化

---
+++ src/main/java/com/mersory/algorithm/sort/InsertSort.java
@@ -0,0 +1,10 @@
+package com.mersory.algorithm.sort;
+
+/**
+ * Description:
+ * Author:         Serry Mei
+ * Email:          meixy@elab-plus.com
+ * Date:           2018/5/23 20:42
+ */
+public class InsertSort {
+}
